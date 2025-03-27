aeebc9f6f79cfec6a516d88fb473352009621ae7
Rauf555822 <raufabdurhimov@gmail.com>
2025-03-27 21:45:10 +0400

    ders18

---
+++ src/main/java/ders18/Dog.java
@@ -0,0 +1,8 @@
+package main.java.ders18;
+
+public class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog: Woof! Woof!");
+    }
+}
---
+++ src/main/java/ders18/Main.java
@@ -0,0 +1,11 @@
+package main.java.ders18;
+
+public class Main {
+    public static void main(String[]args) {
+        Dog Dog = new Dog();
+        Cat Cat = new Cat();
+
+        Dog.makeSound();
+        Cat.makeSound();
+    }
+}
