707a86cce8b2bb1c46d26c8de97308ac3bcf1c64
Rauf555822 <raufabdurhimov@gmail.com>
2025-04-12 15:55:09 +0400

    ders22

---
+++ src/main/java/ders22/LibrarySystem.java
@@ -0,0 +1,33 @@
+package main.java.ders22;
+
+import java.util.*;
+
+public class LibrarySystem {
+    public static void main (String[]args) {
+        List<String> books = new ArrayList<>();
+        books.add("Kavgam");
+        books.add("Silahlara veda");
+        books.add("Beyaz olum: Simo Hayha");
+        books.add("Qaranliqdaki beyaz adam");
+        books.add("Muhendis komputer qrafikasi");
+        books.add("Tetbiqi mexanika");
+        books.add("Riyazi analiz-2");
+        books.add("Oxford Solutions Third Edition Intermediate");
+        books.add("Kavgam");
+        books.add("Beyaz olum: Simo Hayha");
+
+        Set<String> uniqueBooks = new HashSet<>(books);
+
+        Map<String, String>
+                bookCategories = new HashMap<>();
+        bookCategories.put("Kavgam", "bedii");
+        bookCategories.put("SIlahlara veda", "bedii");
+        bookCategories.put("Beyaz olum: Simo Hayha", "bedii");
+        bookCategories.put("Qaranliqdaki beyaz adam", "bedii");
+        bookCategories.put("Muhendis komputer qrafikasi", "universitet dersliyi");
+        bookCategories.put("Tetbiqi mexanika", "universitet dersliyi");
+        bookCategories.put("Riyazi analiz-2", "universitet dersliyi");
+        bookCategories.put("Oxford Solutions Third Edition Intermediate", "universitet dersliyi");
+        System.out.println(bookCategories);
+    }
+}
